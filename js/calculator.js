4697a2ac96af78d3c525f3d762645b1a94009656
master1588 <master1588@gmail.com>
2015-03-28 14:01:24 -0700

    JSON data structure for the calculator
    Comment describing the JSON data going to the calculator from the page.

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,48 @@
-function doCal(jsonData, whatChanged) {
+/**
+ * 
+ * @param {String}   jsonData   Format:
+ *                              expectedFromSSN,
+ *                              annualDesiredIncome:
+ *                                  retirementInterestRate,
+ *                                  incomeFromAge,
+ *                                  incomeToAge,
+ *                              savingsInfo:
+ *                                  savingInterestRate,
+ *                                  fromAge,
+ *                                  toAge
+ *                              assumptions:
+ *                                  initInvestments,
+ *                                  avgTaxRate,
+ *                                  interestRate,
+ *                                  capitalGains,
+ *                                  taxFreeIncome,
+ *                                  dividends
+ *     sample:  
+ *              {"expectedFromSSN":"300",
+ *               "annualDesiredIncome":{
+ *                                      "retirementInterestRate":"3.5",
+ *                                      "incomeFromAge":"65",
+ *                                      "incomeToAge":"95"
+ *                                      },
+ *               "savingsInfo":{
+ *                              "savingInterestRate":"0.6",
+ *                              "fromAge":"40",
+ *                              "toAge":"65"
+ *                              },
+ *               "assumptions":{
+ *                              "initInvestments":"10000",
+ *                              "avgTaxRate":"33.3",
+ *                              "interestRate":".5",
+ *                              "capitalGains":"20.0",
+ *                              "taxFreeIncome":"0",
+ *                              "dividends":"20"
+ *                              }
+ *               }
+ *
+ * @returns {String}            JSON string = monthlyRequiredSavings
+ */
+
+function doCal(jsonData) {
     var data = JSON.parse(jsonData);
     
     var ssn = data.expectedFromSSN;
