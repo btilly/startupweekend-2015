3f289ea562777449dd35d17ce67c5d0fc154d3e0
Kate Yoak <kate@yoak.com>
2015-03-29 17:28:04 +0000

    google analytics

--- js/events.js
+++ js/events.js
@@ -14,11 +14,7 @@ $(function(){
 		};
 		var data = {
                                annualDesiredIncome: {
-<<<<<<< HEAD
-                                   annualIncome: form_value('desired')*1000,
-=======
                                    annualIncome: form_value('desired') * 1000,
->>>>>>> 06a6b9a5ba9cfa6953af7cc9b9bda4a99ce84d93
                                    incomeFromAge: form_value('myage'),
                                    incomeToAge: form_value('lifespan')
 				},
@@ -48,3 +44,12 @@ $(function(){
 		}
 	});
 });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-61310812-1', 'auto');
+  ga('send', 'pageview');
+
